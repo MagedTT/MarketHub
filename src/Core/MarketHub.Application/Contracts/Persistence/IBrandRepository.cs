ea0046a28ca1dfff94d60ca5c4860bfda6bc5a86
MagedTT <maged922001@gmail.com>
2026-06-20 19:54:32 +0300

    Add Methods Signatures to IBrandRepository Contract

--- src/Core/MarketHub.Application/Contracts/Persistence/IBrandRepository.cs
+++ src/Core/MarketHub.Application/Contracts/Persistence/IBrandRepository.cs
@@ -1,6 +1,17 @@
+using MarketHub.Application.Shared;
+using MarketHub.Domain.Entities;
+
 namespace MarketHub.Application.Contracts.Persistence;
 
 public interface IBrandRepository
 {
+    Task<Brand> GetBrandByIdAsync(Guid id, bool trackChanges);
+    Task<PagedList<Brand>> GetBrandsAsync(BrandParameters brandParameters, bool trackChanges);
+    Task<PagedList<string>> GetBrandsNamesAsync(BrandParameters brandParameters, bool trackChanges);
+
+    Task AddBrandAsync(Brand brand);
     Task<bool> CheckBrandExistsAsync(Guid id);
+
+    Task DeleteBrandAsync(Brand brand);
+    Task DeleteBrandAsync(Guid id);
 }
