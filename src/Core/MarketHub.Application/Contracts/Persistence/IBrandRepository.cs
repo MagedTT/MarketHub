8839a598325e6da8120498b324c570f30b5802d4
MagedTT <maged922001@gmail.com>
2026-06-20 20:21:10 +0300

    Modify IBrandRepository Contract by making AddBrand and DeleteBrand synchronous and GetBrandByIdAsync return Nullable brand, and Add Implementation for the contract in BrandRepository in Persistence layer

--- src/Core/MarketHub.Application/Contracts/Persistence/IBrandRepository.cs
+++ src/Core/MarketHub.Application/Contracts/Persistence/IBrandRepository.cs
@@ -5,13 +5,13 @@ namespace MarketHub.Application.Contracts.Persistence;
 
 public interface IBrandRepository
 {
-    Task<Brand> GetBrandByIdAsync(Guid id, bool trackChanges);
+    Task<Brand?> GetBrandByIdAsync(Guid id, bool trackChanges);
     Task<PagedList<Brand>> GetBrandsAsync(BrandParameters brandParameters, bool trackChanges);
     Task<PagedList<string>> GetBrandsNamesAsync(BrandParameters brandParameters, bool trackChanges);
 
-    Task AddBrandAsync(Brand brand);
+    void AddBrand(Brand brand);
     Task<bool> CheckBrandExistsAsync(Guid id);
 
-    Task DeleteBrandAsync(Brand brand);
+    void DeleteBrand(Brand brand);
     Task DeleteBrandAsync(Guid id);
 }
--- src/Infrastructure/MarketHub.Persistence/Repositories/BrandRepository.cs
+++ src/Infrastructure/MarketHub.Persistence/Repositories/BrandRepository.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
 using MarketHub.Application.Contracts.Persistence;
+using MarketHub.Application.Shared;
+using MarketHub.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace MarketHub.Persistence.Repositories;
@@ -8,12 +11,75 @@ public class BrandRepository : IBrandRepository
     private readonly MarketHubDbContext _context;
 
     public BrandRepository(MarketHubDbContext context)
+        => _context = context;
+
+    public async Task<Brand?> GetBrandByIdAsync(Guid id, bool trackChanges)
     {
-        _context = context;
+        IQueryable<Brand> brands = _context.Brands;
+
+        if (!trackChanges)
+            brands = brands.AsNoTracking();
+
+        return await brands.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<PagedList<Brand>> GetBrandsAsync(BrandParameters brandParameters, bool trackChanges)
+    {
+        IQueryable<Brand> brands = _context.Brands;
+
+        if (!trackChanges)
+            brands = brands.AsNoTracking();
+
+        if (!string.IsNullOrWhiteSpace(brandParameters.SearchBrandName))
+            brands = brands.Where(brand => brand.Name.Contains(brandParameters.SearchBrandName));
+
+        int count = await brands.CountAsync();
+
+        List<Brand> brandsList = await brands
+            .OrderBy(x => x.Name)
+            .Skip((brandParameters.PageNumber - 1) * brandParameters.PageSize)
+            .Take(brandParameters.PageSize)
+            .ToListAsync();
+
+        return new PagedList<Brand>(brandsList, count, brandParameters.PageNumber, brandParameters.PageSize);
+    }
+
+    public async Task<PagedList<string>> GetBrandsNamesAsync(BrandParameters brandParameters, bool trackChanges)
+    {
+        IQueryable<Brand> brands = _context.Brands;
+
+        if (!trackChanges)
+            brands = brands.AsNoTracking();
+
+        if (!string.IsNullOrWhiteSpace(brandParameters.SearchBrandName))
+            brands = brands.Where(brand => brand.Name.Contains(brandParameters.SearchBrandName));
+
+        int count = await brands.CountAsync();
+
+        List<string> brandNames = await brands
+            .OrderBy(brand => brand.Name)
+            .Skip((brandParameters.PageNumber - 1) * brandParameters.PageSize)
+            .Take(brandParameters.PageSize)
+            .Select(brand => brand.Name)
+            .ToListAsync();
+
+        return new PagedList<string>(brandNames, count, brandParameters.PageNumber, brandParameters.PageSize);
     }
 
     public async Task<bool> CheckBrandExistsAsync(Guid id)
+        => await _context.Brands.AnyAsync(brand => brand.Id.Equals(id));
+
+    public void AddBrand(Brand brand)
+        => _context.Brands.Add(brand);
+
+    public void DeleteBrand(Brand brand)
+        => _context.Brands.Remove(brand);
+
+    public async Task DeleteBrandAsync(Guid id)
     {
-        return await _context.Brands.AnyAsync(brand => brand.Id.Equals(id));
+        Brand? brand = await _context.Brands.FindAsync(id);
+
+        if (brand is not null)
+            _context.Brands.Remove(brand);
     }
 }
