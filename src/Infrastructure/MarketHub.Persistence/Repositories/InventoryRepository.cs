36bcf6873235cb863c7373fcf4bfc7584bbff2b0
MagedTT <maged922001@gmail.com>
2026-06-21 20:10:45 +0300

    Add Task<bool> CheckEnoughQuantityInStockAsync(Guid productId, int quantity) method implementation in InventoryRepository

--- src/Infrastructure/MarketHub.Persistence/Repositories/InventoryRepository.cs
+++ src/Infrastructure/MarketHub.Persistence/Repositories/InventoryRepository.cs
@@ -1,5 +1,6 @@
 using MarketHub.Application.Contracts.Persistence;
 using MarketHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MarketHub.Persistence.Repositories;
 
@@ -13,4 +14,7 @@ public class InventoryRepository : IInventoryRepository
 
     public Task AddAmountToProductAsync(Inventory inventory)
         => Task.FromResult(_context.Inventories.Add(inventory));
+
+    public async Task<bool> CheckEnoughQuantityInStockAsync(Guid productId, int quantity)
+        => await _context.Inventories.AnyAsync(x => x.ProductId == productId && x.AvailableQuantity >= quantity);
 }
